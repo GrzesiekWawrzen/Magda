264d2045fdffa8f14de8576883d7e35fb838cd1c
Grzegorz Wawrzeń <155682191+GrzesiekWawrzen@users.noreply.github.com>
2024-07-24 10:19:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,21 @@ document.getElementById('submitBtn').addEventListener('click', function(event) {
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
 
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName);
+    console.log('Email:', email);
+    console.log('Phone:', phone);
+
     const ContactData = {
         email: email,
         name: `${firstName} ${lastName}`,
         phone: phone,
-        addTags: ["REGISTER"],
         emailOptIn: 1,
         mobileOptIn: 0,
-        monitoringOptIn: 1,
-        standardDetails: {
-            spektakt: "jasimalgosia",
-            gender: "Male"
-        }
+        monitoringOptIn: 1
     };
+
+    console.log('ContactData:', ContactData);
 
     SMApi.setAll(ContactData);
     SMApi.execute('contact').then((result) => {
