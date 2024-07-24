b48bcf46e5c3939e170652f7e8686f53626faed7
Grzegorz Wawrzeń <155682191+GrzesiekWawrzen@users.noreply.github.com>
2024-07-24 10:15:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-document
-  .getElementById("registrationForm")
-  .addEventListener("submit", function (event) {
+document.getElementById('submitBtn').addEventListener('click', function(event) {
     event.preventDefault();
 
-    const firstName = document.getElementById("first_name").value;
-    const lastName = document.getElementById("last_name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
+    const firstName = document.getElementById('first_name').value;
+    const lastName = document.getElementById('last_name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
 
     const ContactData = {
-      email: email,
-      name: `${firstName} ${lastName}`,
-      phone: phone,
-      addTags: ["REGISTER"],
-      emailOptIn: 1,
-      mobileOptIn: 0,
-      monitoringOptIn: 1,
-      standardDetails: {
-        spektakt: "jasimalgosia",
-        gender: "Male",
-      },
+        email: email,
+        name: `${firstName} ${lastName}`,
+        phone: phone,
+        addTags: ["REGISTER"],
+        emailOptIn: 1,
+        mobileOptIn: 0,
+        monitoringOptIn: 1,
+        standardDetails: {
+            spektakt: "jasimalgosia",
+            gender: "Male"
+        }
     };
 
     SMApi.setAll(ContactData);
-    SMApi.execute("contact")
-      .then((result) => {
+    SMApi.execute('contact').then((result) => {
         console.log(result);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  });
+        alert("Rejestracja zakończona sukcesem!");
+    }).catch((error) => {
+        console.error('Error:', error);
+        alert("Wystąpił błąd podczas rejestracji.");
+    });
+});
